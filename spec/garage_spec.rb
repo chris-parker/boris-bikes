de22f041e9932f162c3543ed22aeb99ccd042d93
Chris Parker <cjp214@gmail.com>
2016-03-04 16:23:52 +0000

    finished refactoring van and garage classes with doubles

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -1,12 +1,15 @@
-
-
 require 'garage'
 
 describe Garage do
+
+	let(:bike) {double :bike}
+
+
   it 'should fix broken bikes' do
-    sucky_bikes = [Bike.new]
-    sucky_bikes[0].report_broken
-    subject.fixed(sucky_bikes)
-    expect(sucky_bikes).to all be_working
+  	allow(bike).to receive_messages(report_broken: false, fix: true, working?: true)
+  	sucky_bikes = [bike]
+  	sucky_bikes[0].report_broken
+  	subject.fixed(sucky_bikes)
+  	expect(sucky_bikes).to all be_working
   end
 end
